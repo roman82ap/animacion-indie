3ac5266381eb07e221e365fd05d0d97dc6d5b5d6
roman82ap <roman82ap@gmail.com>
2025-08-22 20:24:02 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,1 @@
-export default function Home() {
-  return (
-    <div className="text-center">
-      <h2 className="text-4xl font-bold mb-4">Bienvenido a Veridion23</h2>
-      <p className="text-lg text-gray-600">
-        Un portal indie para amantes de la animación.
-      </p>
-    </div>
-  )
-}
+import SERIES from "../data/series";
