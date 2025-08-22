e0df37618dcfd2b1a443a2f3b1bc3cfdfc8504ac
roman82ap <roman82ap@gmail.com>
2025-08-22 20:57:18 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,10 @@
-export default function Layout({ children }) {
+import '../styles/globals.css';
+import Layout from '../components/Layout';
+
+export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-gray-800 text-white p-4">
-        <h1>Veridion23</h1>
-      </header>
-      <main className="flex-1">{children}</main>
-      <footer className="bg-gray-900 text-gray-400 p-4 text-center">
-        © 2024 Veridion23. Todos los derechos reservados.
-      </footer>
-    </div>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
