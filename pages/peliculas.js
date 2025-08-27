3c5052bae0b09b0eaa8bffef6f1116b3dffb3919
roman82ap <roman82ap@gmail.com>
2025-08-27 23:58:40 +0200

    Create peliculas.js

---
+++ pages/peliculas.js
@@ -0,0 +1,10 @@
+import TopTabs from "../components/TopTabs";
+
+export default function Page() {
+  return (
+    <div className="mx-auto max-w-6xl px-4 py-6">
+      <TopTabs />
+      <h1 className="text-2xl font-bold">Pronto…</h1>
+    </div>
+  );
+}
