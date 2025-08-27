0f781cdd0da5269ed55049a9998af4a5c6d8a0be
roman82ap <roman82ap@gmail.com>
2025-08-28 00:22:34 +0200

    Update youtube.js

--- data/youtube.js
+++ data/youtube.js
@@ -1,8 +1,8 @@
 // data/youtube.js
 export const YT_VIDEOS = [
-  { id: "Jro1TkqpEXg", title: "Trailer 1" },
-  { id: "lS8Is7zlJC0", title: "Trailer 2" },
-  { id: "7QF7j8axtl4", title: "Trailer 3" },
+  { id: "Jro1TkqpEXg", title: "Holgazanes & Demonios" },
+  { id: "lS8Is7zlJC0", title: "Fuerza Abusiva Trailer 01" },
+  { id: "7QF7j8axtl4", title: "Fuerza Abusiva Trailer 02" },
   { id: "8pr352T1Ae4", title: "El Crujido – Clip" },
 ];
 
