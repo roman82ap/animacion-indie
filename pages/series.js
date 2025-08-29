80d6f75b65783d69960b3052b8893b26d5e9bfb9
roman82ap <roman82ap@gmail.com>
2025-08-29 21:56:11 +0200

    Update series.js

--- pages/series.js
+++ pages/series.js
@@ -1,19 +1,11 @@
 // pages/series.js
-import ContentGrid from "../components/ContentGrid";
+import TypePage, { getStaticPropsForType } from "./_type-page";
 
-export default function SeriesPage({ items }) {
-  return <ContentGrid title="Series" items={items} />;
+export default function Page(props) {
+  return <TypePage {...props} />;
 }
 
 export async function getStaticProps() {
-  // Import dinámico: NO empaqueta 'fs' para el cliente
-  const { getAllWorks } = await import("../lib/server/content");
-  const all = await getAllWorks();
-
-  const items = (all || [])
-    .filter((x) => (x.type || "") === "Serie")
-    .sort((a, b) => a.title.localeCompare(b.title));
-
-  return { props: { items }, revalidate: 60 };
+  return getStaticPropsForType("Serie", "Series");
 }
 
