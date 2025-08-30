2824ce7689924d06501b90dbbec0819f7427870f
roman82ap <roman82ap@gmail.com>
2025-08-30 22:37:20 +0200

    Update series.js

--- pages/series.js
+++ pages/series.js
@@ -1,11 +1,3 @@
-// pages/series.js
 import TypePage, { getStaticPropsForType } from "./_type-page";
-
-export default function Page(props) {
-  return <TypePage {...props} />;
-}
-
-export async function getStaticProps() {
-  // "Serie" debe coincidir con el campo type/tipo en tus JSON
-  return getStaticPropsForType("Serie", "Series");
-}
+export default function Page(props) { return <TypePage {...props} />; }
+export async function getStaticProps() { return getStaticPropsForType("Serie", "Series"); }
