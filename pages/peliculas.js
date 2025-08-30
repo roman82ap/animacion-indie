5452c95a0760148a7fb435922b0c02161f3a0531
roman82ap <roman82ap@gmail.com>
2025-08-30 22:03:40 +0200

    Update peliculas.js

--- pages/peliculas.js
+++ pages/peliculas.js
@@ -1,5 +1,10 @@
+// pages/peliculas.js
 import TypePage, { getStaticPropsForType } from "./_type-page";
-export default function Page(props) { return <TypePage {...props} />; }
+
+export default function Page(props) {
+  return <TypePage {...props} />;
+}
+
 export async function getStaticProps() {
   return getStaticPropsForType("Película", "Películas");
 }
