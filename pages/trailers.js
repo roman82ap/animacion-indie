ad0a3ef8d963553af6667bc002d96c8e48564834
roman82ap <roman82ap@gmail.com>
2025-08-30 22:04:03 +0200

    Update trailers.js

--- pages/trailers.js
+++ pages/trailers.js
@@ -1,5 +1,10 @@
+// pages/trailers.js
 import TypePage, { getStaticPropsForType } from "./_type-page";
-export default function Page(props) { return <TypePage {...props} />; }
+
+export default function Page(props) {
+  return <TypePage {...props} />;
+}
+
 export async function getStaticProps() {
   return getStaticPropsForType("Trailer", "Trailers");
 }
