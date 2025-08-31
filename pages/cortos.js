c0e41dc1263ba189865abcaf7dcf4126f5a30144
roman82ap <roman82ap@gmail.com>
2025-08-31 17:55:01 +0200

    Update cortos.js

--- pages/cortos.js
+++ pages/cortos.js
@@ -1,9 +1,9 @@
-import TypePage, { getStaticPropsForType } from "./_type-page";
+import TypePage, { getStaticPropsForType } from './_type-page';
 
 export default function Page(props) {
   return <TypePage {...props} />;
 }
 
 export async function getStaticProps() {
-  return getStaticPropsForType("Corto", "Cortos");
+  return getStaticPropsForType('Corto', 'Cortos');
 }
