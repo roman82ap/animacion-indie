53bde4023982c246ad6342022379fa91ba5683b1
roman82ap <roman82ap@gmail.com>
2025-08-31 18:24:45 +0200

    Update catalog.js

--- data/catalog.js
+++ data/catalog.js
@@ -7,6 +7,7 @@
       "media": "3D",
       "genres": ["Acción", "Parodia"],
       "thumb": "/banner/FA-Banner-0001.png",
+      "description": "Un grupo elite se encarga de eliminar la amenaza.",
       "episodes": [
         { "title": "Trailer Oficial", "youtubeId": "7QF7j8axtl4" },
         { "title": "Leoric Trailer", "youtubeId": "lS8Is7zlJC0" }
@@ -19,6 +20,7 @@
       "media": "2D",
       "genres": ["Acción", "Drama"],
       "thumb": "/banner/Crujido-Banner-001.png",
+      "description": "Serie animada creada en solitario por @liners_crunch.",
       "episodes": [
         { "title": "Capítulo 1", "youtubeId": "8pr352T1Ae4" }
       ]
@@ -30,6 +32,7 @@
       "media": "Stop-motion",
       "genres": ["Terror", "Suspenso"],
       "thumb": "/banner/HyD-Banner-001.png",
+      "description": "Un grupo de holgazanes es transportado a un mundo de fantasía.",
       "episodes": [
         { "title": "Ep. 1", "youtubeId": "kX0kYhFbrY4" }
       ]
@@ -41,6 +44,7 @@
       "media": "2D",
       "genres": ["Acción", "Comedia", "Aventura"],
       "thumb": "/banner/ORIGIN-Thumb.png",
+      "description": "Un stickman gris intenta pintar un futuro dorado.",
       "episodes": [
         { "title": "GILDEDGUY STORY #0 - Gildedguy Gets Up", "youtubeId": "X7Arrfu9l30" },
         { "title": "GILDEDGUY STORY #1 - Gildedguy vs Fry", "youtubeId": "KFqsbjX8aLs" }
