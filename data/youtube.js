44547f366b880ee2b7c9e4da08d9740b36804355
roman82ap <roman82ap@gmail.com>
2025-09-01 09:02:43 +0200

    Delete data/youtube.js

--- data/youtube.js
+++
@@ -1,9 +0,0 @@
-// data/youtube.js
-export const YT_VIDEOS = [
-  { id: "Jro1TkqpEXg", title: "Holgazanes & Demonios" },
-  { id: "lS8Is7zlJC0", title: "Fuerza Abusiva Trailer 01" },
-  { id: "7QF7j8axtl4", title: "Fuerza Abusiva Trailer 02" },
-  { id: "8pr352T1Ae4", title: "El Crujido – Clip" },
-];
-
-export const ytThumb = (id) => `https://i.ytimg.com/vi/${id}/hqdefault.jpg`;
