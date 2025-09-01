5d2a77293f07adb17b7d226e2ef8d486d2a00949
roman82ap <roman82ap@gmail.com>
2025-09-01 08:59:45 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
 import "../styles/globals.css";
-import Layout from "../components/Layout";
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
